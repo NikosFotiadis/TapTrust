5d4e971bb960403dadca162afd9f140b352dc7a0
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2023-11-18 23:44:04 +0200

    feat: move poll creation under admin

---
+++ packages/nextjs/pages/admin/events/[eventId]/polls/create.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { useParams } from "next/navigation";
+import { useAccount, useContractWrite } from "wagmi";
+import { baseGoerli } from "wagmi/chains";
+import deployedContracts from "~~/contracts/deployedContracts";
+import { schemaUID } from "~~/services/web3/attestationService";
+
+const nowInSeconds = () => Math.round(Date.now() / 1000);
+
+const CreatePoll = () => {
+  const params = useParams();
+
+  const [title, setTitle] = useState("");
+  const [options, setOptions] = useState([""]);
+  const [duration, setDuration] = useState(0);
+  const { address } = useAccount();
+  const { isLoading, write } = useContractWrite({
+    address: deployedContracts[baseGoerli.id as 31337].Voting.address,
+    abi: deployedContracts[baseGoerli.id as 31337].Voting.abi,
+    functionName: "createPoll",
+  });
+
+  const endTs = nowInSeconds() + duration * 60;
+
+  if (!address) return <div>Not logged in</div>;
+
+  const handleSubmit = () => {
+    write({
+      args: [
+        schemaUID,
+        address, //attester
+        BigInt(Number(params.eventId)),
+        title,
+        options,
+        BigInt(endTs),
+      ],
+    });
+  };
+
+  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleChangeDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isNaN(Number(event.target.value))) {
+      setDuration(Number(event.target.value));
+    }
+  };
+
+  const handleChangeOptions = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newOptions = [...options];
+    newOptions[index] = event.target.value;
+    setOptions(newOptions);
+  };
+
+  const addOptionInput = () => {
+    setOptions([...options, ""]);
+  };
+
+  const removeOptionInput = (i: number) => () => {
+    if (options.length <= 1) {
+      setOptions([""]);
+    } else {
+      const newOptions = [...options];
+      newOptions.splice(i, 1);
+      setOptions(newOptions);
+    }
+  };
+
+  const renderOptionsForm = () => {
+    return (
+      <div>
+        {options.map((option, i) => (
+          <div key={i} className="flex">
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs mt-4"
+              value={options[i]}
+              onChange={handleChangeOptions(i)}
+            />
+            <button className="btn mt-4 bg-red-500" onClick={removeOptionInput(i)}>
+              remove
+            </button>
+          </div>
+        ))}
+        <div className="w-full flex mt-4 mb-4">
+          <button className="btn max-w-xs bg-blue-400" onClick={addOptionInput}>
+            Add Option
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">Title</span>
+        </label>
+        <input
+          type="text"
+          className="input input-bordered w-full max-w-xs"
+          value={title}
+          onChange={handleChangeTitle}
+        />
+        <label className="label">
+          <span className="label-text">Options</span>
+        </label>
+        {renderOptionsForm()}
+        <label className="label">
+          <span className="label-text">Duration (minutes)</span>
+        </label>
+        <input
+          type="text"
+          className="input input-bordered w-full max-w-xs"
+          value={duration}
+          onChange={handleChangeDuration}
+        />
+        <div className="w-full flex mt-4 mb-4">
+          <button className="btn w-full max-w-xs bg-blue-400" onClick={handleSubmit}>
+            {isLoading ? "Loading..." : "Create Poll"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePoll;
---
+++ packages/nextjs/pages/admin/index.tsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import React from "react";
+import type { NextPage } from "next";
+import { useMutation } from "wagmi";
+import { z } from "zod";
+import { Header } from "~~/components/Header";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { registerAddresses } from "~~/services/web3/registerAddresses";
+
+const publicKeySchema = z.object({
+  primaryPublicKeyRaw: z.string(),
+  primaryPublicKeyHash: z.string(),
+  secondaryPublicKeyHash: z.string().nullable(),
+  tertiaryPublicKeyHash: z.string().nullable(),
+  address: z.string(),
+  edition_number: z.number(),
+});
+
+const jsonSchema = z.record(publicKeySchema);
+
+// You can use jsonSchema to parse and validate your JSON data.
+
+interface FileReadComponentProps {
+  transformFunction: (data: z.infer<typeof jsonSchema>) => string[];
+  addresses: string[];
+  onAddressesChange: (addresses: string[]) => void;
+}
+
+const FileReadComponent: React.FC<FileReadComponentProps> = props => {
+  const { transformFunction, addresses, onAddressesChange } = props;
+
+  const handleFileRead = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async e => {
+        const text = e.target?.result;
+        try {
+          const json = JSON.parse(text as string);
+          const transformedData = transformFunction(json);
+          onAddressesChange(transformedData);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileRead} accept="application/JSON" />
+      <ul className="rounded-xl ring-1 p-4 my-4">
+        {addresses.map((item, index) => (
+          <li key={index}>
+            <code>{item}</code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const AdminPage: NextPage = () => {
+  const [eoaAddresses, setEoaAddresses] = useState<string[]>([]);
+
+  const { mutate, status } = useMutation({
+    mutationFn: registerAddresses,
+  });
+
+  return (
+    <>
+      <Header />
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-4xl font-bold">TapTrust</span>
+          </h1>
+          <p className="text-center text-lg">To get started, upload a list of HaLo addresses</p>
+          <FileReadComponent
+            transformFunction={schema => Object.values(schema).map(entry => entry.address)}
+            addresses={eoaAddresses}
+            onAddressesChange={setEoaAddresses}
+          />
+
+          {eoaAddresses.length > 0 && (
+            <div className="flex flex-col items-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => {
+                  // console.log("eoaAddresses", eoaAddresses);
+                  mutate(eoaAddresses);
+                }}
+              >
+                {
+                  {
+                    idle: "Register Halo Addresses",
+                    loading: "Registering...",
+                    error: "Error",
+                    success: "Success",
+                  }[status]
+                }
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
