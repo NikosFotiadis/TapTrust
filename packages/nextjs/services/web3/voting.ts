6f17ca150cfc63fef62e718ae82b0766f5d4edfc
MichalisG <michalis3489@gmail.com>
2023-11-19 01:28:02 +0300

    feat: voting page

--- packages/nextjs/services/web3/voting.ts
+++ packages/nextjs/services/web3/voting.ts
@@ -1,9 +1,10 @@
 import { encodeFunctionData } from "viem";
+import { baseGoerli } from "wagmi/chains";
 import deployedContracts from "~~/contracts/deployedContracts";
 
 export const getVotingUoCallData = (attestationId: string, pollId: string, option: number) => {
   return encodeFunctionData({
-    abi: deployedContracts[84531].Voting.abi,
+    abi: deployedContracts[baseGoerli.id as 84531]?.Voting.abi,
     functionName: "vote",
     args: [attestationId, pollId, BigInt(option)],
   });
